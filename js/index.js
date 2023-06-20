a8e971ea83f631cb5e3aa836c651cadf401fcdbe
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2023-06-20 16:33:05 +0200

    maj page frontend mentor

--- js/index.js
+++ js/index.js
@@ -2,6 +2,24 @@ var url = "https://api.adviceslip.com/advice";
 var advice = document.getElementById("advice");
 var dice = document.getElementById("dice");
 var idAdvice = document.getElementById("idAdvice");
+var separations = document.querySelector(".separation");
+var sepMobile = document.querySelector(".separation-mobile");
+if (window.innerWidth < 1300) {
+  separations.style.display = "none";
+  sepMobile.style.display = "block";
+} else {
+  separations.style.display = "block";
+  sepMobile.style.display = "none";
+}
+onresize = function () {
+  if (window.innerWidth < 1300) {
+    separations.style.display = "none";
+    sepMobile.style.display = "block";
+  } else {
+    separations.style.display = "block";
+    sepMobile.style.display = "none";
+  }
+};
 
 // onInit generer un conseil
 fetch(url)
